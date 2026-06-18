68db6590d4c2756285e61142aadc7b4f4b213a4f
Vivien Goncalves <87721563+Vivien60@users.noreply.github.com>
2026-06-18 04:11:27 +0200

    Feat: Check uploaded file conditions (#5)
    * Feat: Check uploaded file conditions
    
    Add media type and size checks.
    Add functional test for this feature.
    
    * Fix : Test: Missing call to parent::setUp()
    
    Erreur remontée par phpstan :
      Line   tests/Functional/UploadMediaTest.php
     ------ -----------------------------------------
      9      Missing call to parent::setUp() method.
              phpunit.callParent

--- src/Entity/Media.php
+++ src/Entity/Media.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\MediaRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: MediaRepository::class)]
 class Media
@@ -26,6 +27,12 @@ class Media
     #[ORM\Column]
     private string $title;
 
+    #[Assert\Image(
+        maxSize: '2M',
+        maxSizeMessage: 'The image is too large ({{ size }} {{ suffix }}). Allowed maximum size is {{ limit }} {{ suffix }}.',
+        uploadIniSizeErrorMessage: 'The image is too large ({{ size }} {{ suffix }}). Allowed maximum size is {{ limit }} {{ suffix }}.',
+        uploadFormSizeErrorMessage: 'The image is too large. Allowed maximum size for input fields is {{ limit }} {{ suffix }}.',
+    )]
     private ?UploadedFile $file = null;
 
     public function getId(): ?int
---
+++ tests/Functional/UploadMediaTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Tests\Functional;
+
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+class UploadMediaTest extends FunctionalTestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    public function testUploadMediaShouldFail(): void
+    {
+        $this->login();
+        $this->get('admin/media/add');
+        $pathImg3Mo = dirname(__FILE__).DIRECTORY_SEPARATOR.'test_toobig.jpg';
+        $this->client->submitForm('Ajouter', [
+            'media[title]' => 'Test image',
+            'media[file]' => new UploadedFile($pathImg3Mo, 'toobig.jpg', 'image/jpeg', null, true),
+        ]);
+        $this->assertResponseStatusCodeSame(422);
+        $this->assertSelectorTextContains('.invalid-feedback', 'Allowed maximum size is 2 MB');
+    }
+
+    public function testUploadMediaShouldSucceed(): void
+    {
+        $this->login();
+        $this->get('admin/media/add');
+        $pathImg3Mo = dirname(__FILE__).DIRECTORY_SEPARATOR.'test_ok.jpg';
+        $this->client->submitForm('Ajouter', [
+            'media[user]' => '1',
+            'media[album]' => '1',
+            'media[title]' => 'Test image',
+            'media[file]' => new UploadedFile($pathImg3Mo, 'ok.jpg', 'image/jpeg', null, true),
+        ]);
+        $this->assertResponseRedirects('/admin/media');
+        $this->get('/portfolio/1');
+        $this->assertResponseIsSuccessful();
+        $this->assertAnySelectorTextContains('.media-title', 'Test image');
+    }
+}
